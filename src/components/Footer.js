41b8501cfec26d28529e03d30fc5bdb9227ff862
Obinna Nnodim <obiknows88@gmail.com>
2020-02-10 16:44:33 -0700

    updated the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ export default () => (
   <div>
     <h2 className="taCenter">
       Follow us{' '}
-      <a href="https://instagram.com/thrivegoldcoast/">@thrivegoldcoast</a>
+      <a href="#">@placesunknown</a>
     </h2>
     <br />
     <InstagramFeed count="8" />
@@ -14,7 +14,7 @@ export default () => (
       <div className="container taCenter">
         <span>
           © Copyright {new Date().getFullYear()} All rights reserved. Crafted by{' '}
-          <a href="https://thriveweb.com.au/">Thrive</a>.
+          <a href="https://imewe.xyz/">IMEWE</a>.
         </span>
       </div>
     </footer>
